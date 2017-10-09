a34fef20a07016014fc622596d81ff1a7e4c6af8
Servio Mora <servio.mora19@gmail.com>
2017-10-09 15:17:42 -0400

    updating repo

--- server/main.js
+++ server/main.js
@@ -2,4 +2,5 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  process.env.MAIL_URL="smtp://postmaster@sandboxf4b0cbcc9b614627a1d5a39ede74bdd7.mailgun.org:9fac75220647bff019c84da17062bea0@smtp.mailgun.org:587";
 });
