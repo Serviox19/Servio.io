63bd9eb4b3df0b28efe19d3fd11c4371e430adb2
Servio Mora <servio.mora19@gmail.com>
2017-10-10 12:05:23 -0400

    started resume page

--- imports/ui/pages/Resume.jsx
+++ imports/ui/pages/Resume.jsx
@@ -8,7 +8,27 @@ class Resume extends Component {
   render() {
     return (
       <div>
-        <h1>Resume Page</h1>
+        <div id="page-content-wrapper">
+          <div className="intro-wrapper">
+            <div className="block">
+              <div className="image"></div>
+              <span className="name"><h2>Servio Mora</h2></span>
+            </div>
+            <div className="block about">
+              <h2 className="title">Full Stack Web Developer</h2> <br />
+              <h2 style={{ alignSelf: 'flex-start' }}>About Me</h2>
+              <p style={{ marginBottom: '2em' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud</p>
+            </div>
+            <div className="block info">
+              <h2>Address</h2>
+              <p>32 Merrill Rd, Clifton NJ 07012</p>
+              <h2>Contact</h2>
+              <p><span>servio.mora19@gmail.com</span> <br /> <span>973-934-5297</span></p>
+              <a target="_blank" href="">www.servio.io</a>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
