bbc0b33fb2b83149ca956c6f04ae3bf89595618a
Servio Mora <servio.mora19@gmail.com>
2017-10-12 14:38:56 -0400

    education and work experience

--- imports/ui/pages/Resume.jsx
+++ imports/ui/pages/Resume.jsx
@@ -24,16 +24,44 @@ class Resume extends Component {
               <h2>Address</h2>
               <p>32 Merrill Rd, Clifton NJ 07012</p>
               <h2>Contact</h2>
-              <p><span>servio.mora19@gmail.com</span> <br /> <span>github.com/erviox19</span> <br /> <span>973-934-5297</span></p>
+              <p><span>servio.mora19@gmail.com</span> <br /> <span><a target="_blank" href="http://github.com/serviox19">github.com/serviox19</a></span> <br /> <span>973-934-5297</span></p>
               <a target="_blank" href="http://servio.io">www.servio.io</a>
             </div>
           </div> {/* end intro-wrapper */}
           <div className="exp-wrapper">
             <div className="education">
               <h1>Education</h1>
+              <div className="block">
+                <h3>Rutgers University</h3>
+                <h5>Rutgers Coding Bootcamp, Full Stack Web Development</h5>
+                <p>New Brunswick NJ</p>
+              </div>
+              <div className="block">
+                <h3>Passaic County Communuty College</h3>
+                <h5>Associates Degree in Information Technology</h5>
+                <p>Passaic County NJ</p>
+              </div>
             </div>
             <div className="work">
               <h1>Work Experience</h1>
+              <div className="wrapper">
+                <div className="block">
+                  <h3>Full Stack Web Developer</h3>
+                  <h5>Ockupy Inc, May 2017 - Present</h5>
+                </div>
+                <div className="block">
+                  <h3>Field Technician</h3>
+                  <h5>Databit, Oct. 2016 - April 2017</h5>
+                </div>
+                <div className="block">
+                  <h3>Web Developer Intern</h3>
+                  <h5>Startup52</h5>
+                </div>
+                <div className="block">
+                  <h3>Deployment Technician</h3>
+                  <h5>Valley National Bank, Dec. 2015 - Apr 2016</h5>
+                </div>
+              </div>
             </div>
           </div> {/* end exp-wrapper */}
         </div>
