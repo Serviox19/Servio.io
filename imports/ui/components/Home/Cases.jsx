17da8a65e788a726ddc7021d68a73c6e67bdd04b
Servio Mora <servio.mora19@gmail.com>
2017-10-29 19:48:03 -0400

    added cases with logos images and hover effects

--- imports/ui/components/Home/Cases.jsx
+++ imports/ui/components/Home/Cases.jsx
@@ -10,24 +10,40 @@ class Cases extends Component {
     return (
       <div className="cases">
         <div className="wrapper">
-          <div className="case">
-            <img src="" className="logo" />
-            <div className="overlay"></div>
-          </div>
-          <div className="case">
-            <img src="" className="logo" />
-            <div className="overlay"></div>
-          </div>
+          <a href="https://maddcoffee.com/" target="_blank" className="case" style={{ backgroundImage: `url('/images/cases/madd.jpg')` }}>
+            <span className="overlay">
+              <div className="bg_img" style={{ backgroundColor: '#8E2051' }}></div>
+            </span>
+            <span className="logo">
+              <img src="/images/logos/madd-logo.png" />
+            </span>
+          </a>
+          <a href="https://shophauterebel.com/" target="_blank" className="case" style={{ backgroundImage: `url('/images/cases/haute.jpg')` }}>
+            <span className="overlay">
+              <div className="bg_img" style={{ backgroundColor: '#F6DC64' }}></div>
+            </span>
+            <span className="logo">
+              <img src="/images/logos/haute-logo.png" />
+            </span>
+          </a>
         </div>
         <div className="wrapper">
-          <div className="case">
-            <img src="" className="logo" />
-            <div className="overlay"></div>
-          </div>
-          <div className="case">
-            <img src="" className="logo" />
-            <div className="overlay"></div>
-          </div>
+          <a href="http://cami.com" target="_blank" className="case" style={{ backgroundImage: `url('/images/cases/cami.jpg')` }}>
+            <span className="overlay">
+              <div className="bg_img" style={{ backgroundColor: '#EF4639' }}></div>
+            </span>
+            <span className="logo">
+              <img src="/images/logos/cami-logo.jpg" />
+            </span>
+          </a>
+          <a href="https://amcreditrepair.com/" target="_blank" className="case" style={{ backgroundImage: `url('/images/cases/am-credit.jpeg')` }}>
+            <span className="overlay">
+              <div className="bg_img" style={{ backgroundColor: '#00C4CC' }}></div>
+            </span>
+            <span className="logo">
+              <img src="/images/logos/cropped-amcredit.png" />
+            </span>
+          </a>
         </div>
       </div>
     );
