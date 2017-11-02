611f927869d9588e0571466038e6d27f74fec3f8
Servio Mora <servio.mora19@gmail.com>
2017-11-01 21:08:24 -0400

    about additions

--- imports/ui/components/Home/About.jsx
+++ imports/ui/components/Home/About.jsx
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
+import Scrollbar from 'smooth-scrollbar';
 
 class About extends Component {
 
   componentDidMount() {
+    //jquery
 
+    var scrollbar = Scrollbar.init(document.querySelector('#home-page'), {
+      'speed':'3',
+      'overscrollEffect':'bounce',
+      'damping':'.05'
+    });
+
+    scrollbar.addListener(function () {
+      if (scrollbar.offset.y > 500) {
+        $('.banner').css({
+          width: '50%',
+          transition: 'all 0.8s ease-in'
+        });
+      }
+    });
   }
+
 
   render() {
     return (
       <div className="about">
-        <div className="page-content-wrapper">
-          <h2>About Section</h2>
+        <div className="banner"></div>
+        <div className="info">
+          <h2>About Section</h2> <br />
+          <h2>Servio Mora</h2>
         </div>
       </div>
     );
--- imports/ui/components/Nav.jsx
+++ imports/ui/components/Nav.jsx
@@ -12,10 +12,10 @@ class Nav extends Component {
         <nav id="navigation">
           <div className="wrapper">
             <ul>
-              <li><a href="/">Home</a></li>
+              <li><a href="/">About</a></li>
               <li><a href="">Cases</a></li>
-              <li><a href="">Resume</a></li>
               <li><a href="">Contact</a></li>
+              <li><a href="">Resume</a></li>
             </ul>
           </div>
         </nav>
