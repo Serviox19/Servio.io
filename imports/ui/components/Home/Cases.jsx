dcdcc3af21acaa0cfef5ba6cfea4eda73ac965a3
Servio Mora <servio.mora19@gmail.com>
2018-01-04 11:35:04 -0500

    cases overlay fixes

--- imports/ui/components/Home/Cases.jsx
+++ imports/ui/components/Home/Cases.jsx
@@ -21,31 +21,35 @@ class Cases extends Component {
         <div id="row2">
           <div className="left">
             <a href="https://maddcoffee.com" target="_blank" className="case" style={{ backgroundImage: `url('/images/cases/madd.jpg')` }}>
-              <span className="overlay">
-                <div className="bg_img" style={{ backgroundColor: '#8E2051' }}></div>
+              <span className="overlay" style={{ backgroundColor: '#8E2051' }}>
+                <div className="bg_img"></div>
+                <div className="logo"></div>
               </span>
             </a>
           </div>
           <div className="right">
             <div id="big-left">
               <a href="http://cami.com" target="_blank" className="case" style={{ backgroundImage: `url('/images/cases/cami.jpg')` }}>
-                <span className="overlay">
-                  <div className="bg_img" style={{ backgroundColor: '#EF4639' }}></div>
+                <span className="overlay" style={{ backgroundColor: '#EF4639' }}>
+                  <div className="bg_img"></div>
+                  <div className="logo"></div>
                 </span>
               </a>
             </div>
             <div id="stacked">
               <div id="top">
                 <a href="https://shophauterebel.com/" target="_blank" className="case" style={{ backgroundImage: `url('/images/cases/haute.jpg')` }}>
-                  <span className="overlay">
-                    <div className="bg_img" style={{ backgroundColor: '#F6DC64' }}></div>
+                  <span className="overlay" style={{ backgroundColor: '#F6DC64' }}>
+                    <div className="bg_img"></div>
+                    <div className="logo"></div>
                   </span>
                 </a>
               </div>
               <div id="bottom">
                 <a href="https://amcreditrepair.com/" target="_blank" className="case" style={{ backgroundImage: `url('/images/cases/am-credit.jpeg')` }}>
-                  <span className="overlay">
-                    <div className="bg_img" style={{ backgroundColor: '#7CCCD2' }}></div>
+                  <span className="overlay" style={{ backgroundColor: '#7CCCD2' }}>
+                    <div className="bg_img"></div>
+                    <div className="logo"></div>
                   </span>
                 </a>
               </div>
@@ -54,8 +58,9 @@ class Cases extends Component {
         </div>
         <div id="row1">
           <a href="https://ockupy.com" target="_blank" className="case" style={{ backgroundImage: `url('/images/cases/ockupy.jpeg')` }}>
-            <span className="overlay">
-              <div className="bg_img" style={{ backgroundColor: '#FFFFFF' }}></div>
+            <span className="overlay" style={{ backgroundColor: '#FFFFFF' }}>
+              <div className="bg_img"></div>
+              <div className="logo"></div>
             </span>
           </a>
         </div>
