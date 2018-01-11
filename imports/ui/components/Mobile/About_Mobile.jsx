c7ebd2b9d9f77e56e1416b5155453e703e77eb61
Servio Mora <servio.mora19@gmail.com>
2018-01-10 20:24:03 -0500

    created mobile components

---
+++ imports/ui/components/Mobile/About_Mobile.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class MobileAbout extends Component {
+
+  componentDidMount() {
+    //jquery
+  }
+
+  render() {
+    return (
+      <div className="mobile_about">
+        <h1>Mobile About</h1>
+      </div>
+    );
+  }
+}
+
+export default MobileAbout;
---
+++ imports/ui/components/Mobile/Cases_Mobile.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class MobileCases extends Component {
+
+  componentDidMount() {
+    //jquery
+  }
+
+  render() {
+    return (
+      <div className="mobile_cases">
+        <h1>Mobile Cases</h1>
+      </div>
+    );
+  }
+}
+
+export default MobileCases;
