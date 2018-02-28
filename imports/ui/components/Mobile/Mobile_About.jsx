c47dd72cd5f13e6ac56b3abc0981952ccbcc4188
Servio Mora <servio.mora19@gmail.com>
2018-02-28 14:09:19 -0500

    cases hover scale effect done, and added mobile under construction

--- imports/ui/components/Mobile/Mobile_About.jsx
+++ imports/ui/components/Mobile/Mobile_About.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 export const MobileAbout = () => {
   return (
     <div id="mobile_about">
-      <h1>Mobile About</h1>
+      <h1>Mobile Under</h1><br />
+      <h1>Construction !</h1>
     </div>
   );
 }
