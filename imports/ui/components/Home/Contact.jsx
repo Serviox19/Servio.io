5b8eeb6d0c0ec6829114699780031300d18fb66d
Servio Mora <servio.mora19@gmail.com>
2018-03-09 21:55:44 -0500

    needed new tab for paypal donate

--- imports/ui/components/Home/Contact.jsx
+++ imports/ui/components/Home/Contact.jsx
@@ -7,11 +7,11 @@ export const Contact = () => {
       <h1 className="contact-title">Let's Chat!</h1>
       <ContactForm />
       <div id="donate" style={{ marginTop: '1.5em' }}>
-        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+        <form target="_blank" action="https://www.paypal.com/cgi-bin/webscr" method="post">
           <input type="hidden" name="cmd" value="_s-xclick" />
           <input type="hidden" name="hosted_button_id" value="VGFCDAQE3UXEA" />
           <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" name="submit" alt="PayPal - Donate" />
-          <img alt="" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+          <img alt="" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" />
         </form>
       </div>
     </div>
