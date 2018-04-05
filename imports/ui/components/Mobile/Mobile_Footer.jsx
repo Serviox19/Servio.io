f4f036db1f1c64e168a8b2ef0e5d44aefc59b359
Servio Mora <servio.mora19@gmail.com>
2018-04-05 14:54:00 -0400

    good mobile nav and footer for now

--- imports/ui/components/Mobile/Mobile_Footer.jsx
+++ imports/ui/components/Mobile/Mobile_Footer.jsx
@@ -3,7 +3,24 @@ import React from 'react';
 export const MobileFooter = () => {
   return (
     <div id="mobile_footer">
-      <h1>mobile footer</h1>
+      <div className="wrapper">
+        <div className="links">
+          <span>
+            <a
+              href="https://github.com/Serviox19"
+              target="_blank">
+              <img src="/images/icons/github.svg" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/servio-m-b93972108/" target="_blank">
+              <img src="/images/icons/linkedin.svg" />
+            </a>
+          </span>
+        </div>
+        <div className="copyright">
+          <p>&copy; 2018 Servio.io</p>
+        </div>
+      </div>
     </div>
   );
 }
